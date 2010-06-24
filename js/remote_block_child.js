ae01ce50780ae29237df9c2da9c0e592a8257327
TechMission <developer@techmission.org>
2010-06-24 14:48:22 -0400

    Added jquery postmessage support for dynamic <iframe> resizing; created <iframe> demo

---
+++ js/remote_block_child.js
@@ -0,0 +1,21 @@
+$(function(){
+  // Get the parent page URL as it was passed in, for browsers that don't support
+  // window.postMessage (this URL could be hard-coded).
+  var parent_url = decodeURIComponent( document.location.hash.replace( /^#/, '' ) ),
+    link;
+  
+  // The first param is serialized using $.param (if not a string) and passed to the
+  // parent window. If window.postMessage exists, the param is passed using that,
+  // otherwise it is passed in the location hash (that's why parent_url is required).
+  // The second param is the targetOrigin.
+  function setHeight() {
+    $.postMessage({ if_height: $('body').outerHeight( true ) }, parent_url, parent );
+  };
+  
+  // Now that the DOM has been set up (and the height should be set) invoke setHeight.
+  setHeight();
+	
+	// Bind to Quicktabs list items so that iframe resizes when they are clicked
+	$('.quicktabs_tabs li').click(function() { setHeight(); });
+  
+});
